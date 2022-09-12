46aeda462d541a11b1ecb5b14c515b1801d90ca8
asharinovaldy <asharinovaldi6319@gmail.com>
2022-09-12 13:32:08 +0800

    create components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,10 @@
 import type { NextPage } from "next";
-import Link from "next/link";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
     <>
-      <header>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link href="/users">Users</Link>
-          </li>
-        </ul>
-      </header>
+      <Header />
       <h1>Welcome Next.JS</h1>
     </>
   );
