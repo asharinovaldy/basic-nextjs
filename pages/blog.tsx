bd44d14bad7c2c4bdefa86184f7b89899afeebd4
asharinovaldy <asharinovaldi6319@gmail.com>
2022-09-14 17:52:31 +0800

    optimize image

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,98 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
 
 export default function Blog() {
   return (
     <Layout pageTitle="Blog Page">
       <p>This is Blog Page</p>
+      <div></div>
+      <Image
+        src="/going-merry.png"
+        alt="Going Merry"
+        width={400}
+        height={400}
+      />
+      <Image src="/food.jpg" width={400} height={400} />
+      <p className="description-image">
+        Image Optimization di Next JS sangat powerful untuk optimasi elemen
+        gambar di halaman website, karena gambar akan di-load ketika gambar akan
+        dibutuhkan. Gambar tersebut diolah menjadi format .webp dan berukuran
+        sangat kecil, hal ini berguna untuk performa halaman website yang lebih
+        cepat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo,
+        veniam ex dolor nihil earum tenetur quidem quod omnis voluptas numquam
+        illum officiis, est repellat aut expedita ea at! Repudiandae totam a
+        porro ipsa sint explicabo? Soluta blanditiis similique impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo,
+        veniam ex dolor nihil earum tenetur quidem quod omnis voluptas numquam
+        illum officiis, est repellat aut expedita ea at! Repudiandae totam a
+        porro ipsa sint explicabo? Soluta blanditiis similique impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo,
+        veniam ex dolor nihil earum tenetur quidem quod omnis voluptas numquam
+        illum officiis, est repellat aut expedita ea at! Repudiandae totam a
+        porro ipsa sint explicabo? Soluta blanditiis similique impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo,
+        veniam ex dolor nihil earum tenetur quidem quod omnis voluptas numquam
+        illum officiis, est repellat aut expedita ea at! Repudiandae totam a
+        porro ipsa sint explicabo? Soluta blanditiis similique impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo,
+        veniam ex dolor nihil earum tenetur quidem quod omnis voluptas numquam
+        illum officiis, est repellat aut expedita ea at! Repudiandae totam a
+        porro ipsa sint explicabo? Soluta blanditiis similique impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo,
+        veniam ex dolor nihil earum tenetur quidem quod omnis voluptas numquam
+        illum officiis, est repellat aut expedita ea at! Repudiandae totam a
+        porro ipsa sint explicabo? Soluta blanditiis similique impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo,
+        veniam ex dolor nihil earum tenetur quidem quod omnis voluptas numquam
+        illum officiis, est repellat aut expedita ea at! Repudiandae totam a
+        porro ipsa sint explicabo? Soluta blanditiis similique impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo,
+        veniam ex dolor nihil earum tenetur quidem quod omnis voluptas numquam
+        illum officiis, est repellat aut expedita ea at! Repudiandae totam a
+        porro ipsa sint explicabo? Soluta blanditiis similique impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo,
+        veniam ex dolor nihil earum tenetur quidem quod omnis voluptas numquam
+        illum officiis, est repellat aut expedita ea at! Repudiandae totam a
+        porro ipsa sint explicabo? Soluta blanditiis similique impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo,
+        veniam ex dolor nihil earum tenetur quidem quod omnis voluptas numquam
+        illum officiis, est repellat aut expedita ea at! Repudiandae totam a
+        porro ipsa sint explicabo? Soluta blanditiis similique impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo,
+        veniam ex dolor nihil earum tenetur quidem quod omnis voluptas numquam
+        illum officiis, est repellat aut expedita ea at! Repudiandae totam a
+        porro ipsa sint explicabo? Soluta blanditiis similique impedit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam illo,
+        veniam ex dolor nihil earum tenetur quidem quod omnis voluptas numquam
+        illum officiis, est repellat aut expedita ea at! Repudiandae totam a
+        porro ipsa sint explicabo? Soluta blanditiis similique impedit?
+      </p>
+      <Image src="/going-merry2.jpg" width={400} height={400} />
     </Layout>
   );
 }
