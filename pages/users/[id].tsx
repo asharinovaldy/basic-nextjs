60e68b7c3389cd66b72864a1e98a9dc299a3ad1d
asharinovaldy <asharinovaldi6319@gmail.com>
2022-09-19 14:44:14 +0800

    dynamic route

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,10 +1,13 @@
-import Head from "next/head";
 import Layout from "../../components/Layout";
+import { useRouter } from "next/router";
 
 export default function UserDetail() {
+  const router = useRouter();
+  const { id } = router.query;
+
   return (
     <Layout pageTitle="User Detail Page">
-      <p>This is User Detail Page</p>
+      <p>This is User {id}</p>
     </Layout>
   );
 }
